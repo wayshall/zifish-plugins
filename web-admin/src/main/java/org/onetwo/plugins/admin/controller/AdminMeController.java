61c390fb5a3abd0a23653abc6a3328318d74c860
wayshall <pistols@qq.com>
2019-05-28 01:34:56 +0800

    fix adminme

--- web-admin/src/main/java/org/onetwo/plugins/admin/controller/AdminMeController.java
+++ web-admin/src/main/java/org/onetwo/plugins/admin/controller/AdminMeController.java
@@ -1,11 +1,13 @@
 package org.onetwo.plugins.admin.controller;
 
-import lombok.Data;
+import java.util.List;
 
 import org.onetwo.common.spring.copier.CopyUtils;
 import org.onetwo.common.web.userdetails.UserDetail;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.Data;
 
 @RestController
 public class AdminMeController extends WebAdminBaseController {
@@ -25,6 +27,7 @@ public class AdminMeController extends WebAdminBaseController {
 		String nickName;
 		String userName;
 		String avatar;
+		List<String> roles;
 	}
 
 }
