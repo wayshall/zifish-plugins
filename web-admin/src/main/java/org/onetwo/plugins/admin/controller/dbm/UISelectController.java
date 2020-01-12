3afc6c86847f003b10f2d7bb7f03898849b1aa0e
wayshall <weishao.zeng@gmail.com>
2020-01-12 23:28:38 +0800

    fix dbm ui controller

--- web-admin/src/main/java/org/onetwo/plugins/admin/controller/dbm/UISelectController.java
+++ web-admin/src/main/java/org/onetwo/plugins/admin/controller/dbm/UISelectController.java
@@ -1,7 +1,7 @@
 package org.onetwo.plugins.admin.controller.dbm;
 
 import org.onetwo.boot.core.web.controller.AbstractBaseController;
-import org.onetwo.dbm.ui.spi.UISelectDataProviderService;
+import org.onetwo.dbm.ui.spi.DUISelectDataProviderService;
 import org.onetwo.dbm.ui.vo.UISelectDataRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
@@ -15,11 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/dbm/uiselect")
-@ConditionalOnBean(UISelectDataProviderService.class)
+@ConditionalOnBean(DUISelectDataProviderService.class)
 public class UISelectController extends AbstractBaseController {
 	
 	@Autowired
-	private UISelectDataProviderService selectDataProviderService;
+	private DUISelectDataProviderService selectDataProviderService;
 	
 	@GetMapping(path = "dataProvider")
 	public Object dataProvider(UISelectDataRequest request) {
