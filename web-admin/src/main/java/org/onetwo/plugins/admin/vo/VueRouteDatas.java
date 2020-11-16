c728eb48f4a80436c195ab15e8318276db4475dd
wayshall <weishao.zeng@gmail.com>
2020-11-16 23:20:21 +0800

    fix router

--- web-admin/src/main/java/org/onetwo/plugins/admin/vo/VueRouteDatas.java
+++ web-admin/src/main/java/org/onetwo/plugins/admin/vo/VueRouteDatas.java
@@ -20,6 +20,22 @@ abstract public class VueRouteDatas {
 		return Layout;
 	}
 	
+	/***
+	 * 创建多级菜单时，为了避免重复嵌套侧栏和顶栏，父菜单不能使用Layout，需要重写一个模板页：
+	 * <template>
+  		<router-view/>
+	   </template>
+	 * @author weishao zeng
+	 * @return
+	 */
+	final public static RouteData routerView() {
+		RouteData menuNote = new RouteData("", null);
+		menuNote.setTemplate("<template>" + 
+							 "  <router-view/>" + 
+							 "</template>");
+		return menuNote;
+	}
+	
 	final public static RouteData componentView(String componentViewPath) {
 		return new RouteData(componentViewPath, null);
 	}
--- web-admin/src/main/java/org/onetwo/plugins/admin/vo/VueRouterTreeModel.java
+++ web-admin/src/main/java/org/onetwo/plugins/admin/vo/VueRouterTreeModel.java
@@ -8,16 +8,44 @@ import org.onetwo.common.utils.LangUtils;
 import org.onetwo.common.utils.StringUtils;
 import org.onetwo.common.web.utils.RequestUtils;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.google.common.collect.Maps;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
+ * 见：https://panjiachen.gitee.io/vue-element-admin-site/zh/guide/essentials/router-and-nav.html#%E9%85%8D%E7%BD%AE%E9%A1%B9
+ * 
+ * // 当设置 true 的时候该路由不会在侧边栏出现 如401，login等页面，或者如一些编辑页面/edit/1
+hidden: true // (默认 false)
+
+//当设置 noRedirect 的时候该路由在面包屑导航中不可被点击
+redirect: 'noRedirect'
+
+// 当你一个路由下面的 children 声明的路由大于1个时，自动会变成嵌套的模式--如组件页面
+// 只有一个时，会将那个子路由当做根路由显示在侧边栏--如引导页面
+// 若你想不管路由下面的 children 声明的个数都显示你的根路由
+// 你可以设置 alwaysShow: true，这样它就会忽略之前定义的规则，一直显示根路由
+alwaysShow: true
+
+name: 'router-name' // 设定路由的名字，一定要填写不然使用<keep-alive>时会出现各种问题
+meta: {
+  roles: ['admin', 'editor'] // 设置该路由进入的权限，支持多个权限叠加
+  title: 'title' // 设置该路由在侧边栏和面包屑中展示的名字
+  icon: 'svg-name' // 设置该路由的图标，支持 svg-class，也支持 el-icon-x element-ui 的 icon
+  noCache: true // 如果设置为true，则不会被 <keep-alive> 缓存(默认 false)
+  breadcrumb: false //  如果设置为false，则不会在breadcrumb面包屑中显示(默认 true)
+  affix: true // 若果设置为true，它则会固定在tags-view中(默认 false)
+
+  // 当路由设置了该属性，则会高亮相对应的侧边栏。
+  // 这在某些场景非常有用，比如：一个文章的列表页路由为：/article/list
+  // 点击文章进入文章详情页，这时候路由为/article/1，但你想在侧边栏高亮文章列表的路由，就可以进行如下设置
+  activeMenu: '/article/list'
+}
  * @author wayshall
  * <br/>
  */
@@ -26,6 +54,7 @@ import lombok.Setter;
 public class VueRouterTreeModel extends AbstractTreeModel<VueRouterTreeModel> {
 	@Getter
 	@Setter
+	@JsonIgnore
 	private String url;
 	//menu is false, permission is true
 	private boolean hidden;
@@ -48,6 +77,11 @@ public class VueRouterTreeModel extends AbstractTreeModel<VueRouterTreeModel> {
 		return meta;
 	}
 	
+	/***
+	 * 对应着路由的component属性
+	 * @author weishao zeng
+	 * @return
+	 */
 	public String getPath() {
 		// 如果是外部链接，直接返回链接即可
 		if (RequestUtils.isHttpPath(url)) {
@@ -161,12 +195,21 @@ public class VueRouterTreeModel extends AbstractTreeModel<VueRouterTreeModel> {
 		this.router = (RouteData)this.meta.remove("router");
 	}
 	
+	public String getTemplate() {
+		return this.router.getTemplate();
+	}
+	
 	@Data
-	@AllArgsConstructor
 	@NoArgsConstructor
 	public static class RouteData {
 		String componentViewPath;
 		Map<String, Object> props;
+		String template;
+		public RouteData(String componentViewPath, Map<String, Object> props) {
+			super();
+			this.componentViewPath = componentViewPath;
+			this.props = props;
+		}
 	}
 	
 
