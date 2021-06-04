7243ad188c6acf9c28d7a64e0090baf229e4ecf0
weishao zeng <weishao.zeng@gmail.com>
2021-06-04 15:41:33 +0800

    fix: add permisson

--- web-admin/src/main/java/org/onetwo/plugins/admin/controller/dbm/UIEntityMetaController.java
+++ web-admin/src/main/java/org/onetwo/plugins/admin/controller/dbm/UIEntityMetaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.onetwo.boot.core.web.controller.AbstractBaseController;
 import org.onetwo.dbm.ui.core.DefaultUIEntityMetaService;
 import org.onetwo.dbm.ui.vo.SearchableFieldVO;
+import org.onetwo.ext.permission.api.annotation.ByPermissionClass;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,7 @@ public class UIEntityMetaController extends AbstractBaseController {
 	@Autowired
 	private DefaultUIEntityMetaService entityMetaService;
 	
+	@ByPermissionClass
 	@GetMapping(path = "getSearchFields")
 	public List<SearchableFieldVO> getSearchFields(String entity) {
 		List<SearchableFieldVO> fields = entityMetaService.getSearchFields(entity);
