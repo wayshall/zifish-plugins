0d985d7b0b1c8261af2e2fb35721b0414fd845e0
weishao zeng <weishao.zeng@gmail.com>
2021-08-21 17:55:45 +0800

    fix: fix security
    fix https://github.com/wayshall/onetwo/issues/66

---
+++ web-admin/src/test/java/org/onetwo/plugins/admin/AdminTest.java
@@ -0,0 +1,17 @@
+package org.onetwo.plugins.admin;
+/**
+ * @author weishao zeng
+ * <br/>
+ */
+
+import org.junit.Test;
+import org.onetwo.plugins.admin.utils.WebAdminPermissionConfig.RootMenuClassProvider;
+
+public class AdminTest {
+	
+	@Test
+	public void test() {
+		System.out.println(RootMenuClassProvider.class.getName());
+	}
+
+}
